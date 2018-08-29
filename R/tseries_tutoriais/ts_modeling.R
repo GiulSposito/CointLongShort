495787408427c62091d0208bef76d8209cd79824
Giuliano Sposito <gsposito@ciandt.com>
2018-08-29 18:03:20 -0300

    notacoes do tutorial sobre t-series

---
+++ R/tseries_tutoriais/ts_modeling.R
@@ -0,0 +1,73 @@
+# A Complete Tutorial on Time Series Modeling in R 
+# https://www.analyticsvidhya.com/blog/2015/12/complete-tutorial-time-series-modeling/
+
+# Stationary Definition
+#
+# 1. the mean of the series should not be a function of time rather should be a constant
+# 2. the variance of the series should not be a function of time (homoscedasticity)
+# 3. the covariance of the i_th term and the (i+m)_th term should not be a function of time ("the pace")
+#
+# Unless your time series is stationary, you cannot build a time series model!!
+
+# Random walks:
+#
+# the next position of a value is only dependent of the current position
+# x(t) = x(t-1) + Er(t)
+# 
+# expaning:
+# x(t) => x(0) + sum(Er(1)...Er(t))
+#
+# is the mean constant?
+# E[x(t)] = E[x(0)] + sum(E[er(1)]...E[ef(t)])
+#
+# The "expectation" of a error is zero if error is random
+# E[x(t)] = E[x(0)]
+#
+# Is the variance constant?
+#  Var[x(t)] = Var[x(0)] + sum(Var[er(1)]...Var[ef(t)])
+#  Var[x(t)] = t * Var(error) ==> Time Dependent # not stationary
+
+# Rho Coefficient
+#
+# x(t) = Rho * x(t-1) + Er(t)
+#
+# Rho = 1   ==> not stationary
+# Rho = 0.1 ==> almost stationary
+# Rho = 0   ==> perfectly stationary ==> x(t) = Er(t)
+#
+# Thaking the Expectation
+# E[x(t)] = Rho * E[x(t-1)]
+# The next X (or at time point t) is being pulled down to Rho * Last value of X
+# if X is 1 no force can pull the X down in the next step
+#
+# Dickey Fuller
+#
+# X(t) = Rho * X(t-1) + Er(t)
+# X(t)-X(t-1) = (Rho-1)X(t-1) + Er(t) ==> delta.x = (Rho-1)lag.x + Er(t)
+# we have to test if (Rho-1) is significantly different than zero
+# if null hypothesis gets rejected ==> stationary
+
+
+# data
+data(AirPassengers)
+class(AirPassengers)
+
+# check
+str(AirPassengers)
+start(AirPassengers)
+end(AirPassengers)
+frequency(AirPassengers)
+summary(AirPassengers)
+
+# plot
+plot(AirPassengers)
+abline(reg=lm(AirPassengers~time(AirPassengers)), col="red")
+
+# months
+cycle(AirPassengers)
+
+# accumulated
+plot(aggregate(AirPassengers, FUN = mean))
+
+# by month
+boxplot(AirPassengers~cycle(AirPassengers))
