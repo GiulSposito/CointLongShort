6eec5f3bd04ec6503988b0095ba257adbc01986b
Giuliano Sposito <gsposito@ciandt.com>
2018-08-31 22:29:02 -0300

    Separando funcoes em arquivo proprio

--- R/coint_analysis/ufrgs_analysis.R
+++ R/coint_analysis/ufrgs_analysis.R
@@ -4,6 +4,7 @@ library(BatchGetSymbols)
 library(broom)
 library(tseries)
 library(urca)
+source("./R/coint_analysis/coint_lib.R")
 
 # range da analise
 start.date <- ymd(20050101)
@@ -88,61 +89,6 @@ pairs %>%
   inner_join( nested.prices %>% set_names(c("ticker.b","prices.b")), by = "ticker.b" ) %>% 
   select(ticker.a, prices.a, ticker.b, prices.b) -> dtset
 
-
-# function para fazer o fit linear te dois ativos
-# price.adjusted.A = f(price.adjusted.B)
-fitLinModel <- function(a,b){
-  a %>%
-    select(ref.date, price.a = price.adjusted) %>%
-    inner_join(b %>% select(ref.date, price.b = price.adjusted), by="ref.date") %>% 
-    lm(price.a ~ price.b, .) %>% 
-    return()
-}
-
-# aplica o teste de Dickey-Fuller
-testADF <- function(m) suppressWarnings({
-  m$residuals %>% 
-    adf.test(.,"stationary",k=1) %>% 
-    tidy() %>% 
-    select(-alternative, -method, -parameter) %>% 
-    set_names(paste0("adf.",names(.))) %>% 
-    return()
-})
-
-# aplica Dickey-Fuller usando o URCA package
-testURDF <- function(m){
-  m$residuals %>% 
-    ur.df(type="drift", lags=1) %>% 
-    return()
-}
-
-# devolve o cálculo de meia vida tirado do urca::ur.df
-urdfHalfLife <- function(urdf){
-  urdf %>% summary() -> m
-  half.life <- -log(2)/m@testreg$coefficients[2]
-  return(half.life)
-}
-
-# devolve o cálculo de meia vida tirado do tseries::adf.test
-calcHalfLife <- function(m){
-  tibble(y = m$residuals) %>% 
-    mutate(
-      y.lag   = dplyr::lag(y,1),
-      delta.y = c(NA,diff(y))
-    ) %>% 
-    filter( complete.cases(.) ) %>% 
-    lm(delta.y ~ y.lag, data=.) %>% 
-    summary() -> regress
-  
-  lambda  <- regress$coefficients[2]
-  half.life <- -log(2)/lambda
-  return(half.life)
-}
-
-# calcula a largura do canal (2*spread)
-spreadSize <- function(m){
-  return(2*sd(m$residuals))
-}
 
 # calculos
 dtset %>% 
